53165f6b9cc61bef224aa22bf3a26e8ac8858d2f
Kakul Mittal <kakulmittal2006@gmail.com>
2026-05-16 21:24:09 +0530

    Create Implement queue using Stack LC 232 .java

---
+++ stack/Implement queue using Stack LC 232 .java
@@ -0,0 +1,39 @@
+class MyQueue {
+    private Stack<Integer> sin;
+    private Stack<Integer> sout;
+
+    public MyQueue() {
+        sin= new Stack<>();
+        sout= new Stack<>();
+        
+    }
+  public void push(int x) {
+        sin.push(x);
+        
+    }
+  public int pop() {
+        peek();
+        return sout.pop();
+        
+    }
+  public int peek() {
+        if(sout.isEmpty()){
+            while(!sin.isEmpty()){
+                sout.push(sin.pop());
+            }
+        }
+        return sout.peek();
+  }
+  public boolean empty() {
+        return sin.isEmpty() && sout.isEmpty();
+  }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
