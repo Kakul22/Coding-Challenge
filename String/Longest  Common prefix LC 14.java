5e6adfb1beabe7fb9af89b2c690691ece670a763
Kakul Mittal <kakulmittal2006@gmail.com>
2026-08-02 23:25:50 +0530

    Create Longest  Common prefix LC 14.java

---
+++ String/Longest  Common prefix LC 14.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String prefix = strs[0];
+        for (int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty())
+                    return "";
+            }
+        }
+        return prefix;
+    }
+}
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
