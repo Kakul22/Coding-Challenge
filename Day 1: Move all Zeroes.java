5ed542a6c35638d688114951f6095208876a2061
Kakul Mittal <kakulmittal2006@gmail.com>
2026-08-07 16:50:53 +0530

    Update Day 1: Move all Zeroes.java

--- Day 1: Move all Zeroes.java
+++ Day 1: Move all Zeroes.java
@@ -1,16 +1,16 @@
 #move all zeroes to end 
 class Solution {
     public void moveZeroes(int[] nums) {
-        int count = 0;
-        int temp;                   
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {  
-                temp = nums[i];              # swapping all the non zero numbers first
-                nums[i] = nums[count];
-                nums[count] = temp;
-                count++;
+        int n=nums.length;
+        for(int i=0,j=0;j<n;j++){
+            if(nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
             }
         }
-
+        
+        
     }
 }
